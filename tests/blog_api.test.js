cc29f6e8b56bdc2540bca96c1b43e4cb7e24f635
Juani-tech <jperezdichiazza@gmail.com>
2024-04-10 12:19:05 -0300

    part 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -16,17 +16,18 @@ beforeEach(async () => {
   }
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-}, 100000);
-
 test("all blogs are returned", async () => {
   const response = await api.get("/api/blogs");
 
   expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+test("blogs have an id", async () => {
+  const response = await api.get("/api/blogs");
+
+  const blog = response.body[0];
+
+  expect(blog.id).toBeDefined();
 });
 
 afterAll(() => {
