69472393b64a79be32c1baed470ca61ecbdda75d
Juani-tech <jperezdichiazza@gmail.com>
2024-05-21 15:30:37 -0300

    4.22. Added extractUser as middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,17 +2,17 @@ const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const middleware = require("../utils/middleware");
 
 blogsRouter.get("/", async (request, response) => {
   const blog = await Blog.find({}).populate("user");
   response.json(blog);
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", middleware.userExtractor, async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
+  if (!request.user) {
     return response.status(401).json({ error: "token missing or invalid" });
   }
 
@@ -20,40 +20,46 @@ blogsRouter.post("/", async (request, response) => {
     response.status(400).end();
   }
 
-  const users = await User.find({});
-  // Por ahora dejo el primer usuario
-  const selectedUser = users[0];
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: selectedUser._id,
+    user: request.user._id,
   });
 
   const savedBlog = await blog.save();
-  selectedUser.blogs = selectedUser.blogs.concat(savedBlog._id);
-  await selectedUser.save();
+  request.user.blogs = request.user.blogs.concat(savedBlog._id);
+  await request.user.save();
 
   response.status(201).json(savedBlog);
 });
 
-blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+blogsRouter.delete(
+  "/:id",
+  middleware.userExtractor,
+  async (request, response) => {
+    if (!request.user) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
 
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+    const blog = await Blog.findById(request.params.id);
+
+    if (blog.user.toString() !== request.user.id) {
+      return response.status(401).json({ error: "unauthorized user" });
+    }
+
+    await Blog.findByIdAndDelete(request.params.id);
+
+    const user = await User.findById(request.user.id);
+    user.blogs = user.blogs.filter(
+      (blog) => blog.id.toString() !== request.params.id
+    );
+    await user.save();
+
+    response.status(204).end();
   }
-
-  const blog = await Blog.findById(request.params.id);
-
-  if (blog.user.toString() !== decodedToken.id) {
-    return response.status(401).json({ error: "unauthorized user" });
-  }
-
-  await Blog.findByIdAndDelete(request.params.id);
-  response.status(204).end();
-});
+);
 
 blogsRouter.put("/:id", async (request, response) => {
   const body = request.body;
